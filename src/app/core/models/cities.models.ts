742863cfe5aa5b1cedb7928931878a8f2d9debdd
Tim Kraken <tim.moyence@gmail.com>
2024-04-10 09:05:39 +0200

    create models

---
+++ src/app/core/models/cities.models.ts
@@ -0,0 +1,11 @@
+export interface cities {
+  id: number;
+  name: string;
+  country: string;
+  latitude: number;
+  longitude: number;
+  population: number;
+  language: string;
+  currency: string;
+  image: string;
+}
---
+++ src/app/core/models/formCity.models.ts
@@ -0,0 +1,12 @@
+export class FormCity {
+  id: number;
+  name: string;
+  country: string;
+  latitude: number;
+  longitude: number;
+  population: number;
+  language: string;
+  cygle: string;
+  monnaie: string;
+  image: string;
+}
