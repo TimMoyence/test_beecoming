fbec4ae711df044f759171bd131be6cd677967bc
Tim Kraken <tim.moyence@gmail.com>
2024-04-10 09:04:22 +0200

    create maps page

--- src/app/views/maps/maps.component.ts
+++ src/app/views/maps/maps.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { LatLngTuple } from 'leaflet';
+import { CapitalsGetaway } from '../../core/adapters/capitals.getaway';
+
 @Component({
   selector: 'app-maps',
   standalone: true,
@@ -8,10 +11,57 @@ import * as L from 'leaflet';
   styleUrl: './maps.component.css',
 })
 export class MapsComponent implements OnInit {
+  myIcon = L.icon({
+    iconUrl: 'assets/my-icon.png',
+    iconSize: [30, 55],
+    iconAnchor: [18, 45],
+    popupAnchor: [-3, -76],
+  });
+
+  myposition = L.icon({
+    iconUrl: 'assets/my-position.png',
+    iconSize: [30, 55],
+    iconAnchor: [18, 45],
+    popupAnchor: [-3, -76],
+  });
+
+  map: L.Map;
+
+  constructor(private capitalsGetaway: CapitalsGetaway) {}
+
   ngOnInit() {
-    const map = L.map('map').setView([51.505, -0.09], 13);
+    this.map = L.map('map').setView([0, 0], 2);
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors',
-    }).addTo(map);
+    }).addTo(this.map);
+
+    this.capitalsGetaway.getCapitals().subscribe((capitals) => {
+      capitals.forEach((capital) => {
+        const LatLng = [capital.latitude, capital.longitude];
+        const popupContent = `
+          <div>
+            <h3>${capital.name}</h3>
+            <p><strong>Pays:</strong> ${capital.country}</p>
+            <p><strong>Population:</strong> ${capital.population}</p>
+          </div>
+        `;
+        L.marker(LatLng as LatLngTuple, { icon: this.myIcon })
+          .addTo(this.map)
+          .bindPopup(popupContent);
+      });
+    });
+  }
+
+  locateUser() {
+    this.map.locate({ setView: true, maxZoom: 2 });
+
+    this.map.on('locationfound', (e) => {
+      L.marker(e.latlng, { icon: this.myposition }).addTo(this.map);
+    });
+
+    this.map.on('locationerror', (e) => {
+      alert(e.message);
+    });
   }
 }
