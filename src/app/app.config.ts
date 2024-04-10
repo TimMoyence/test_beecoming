94079af1ffbe19b54f6edf74d5bea725de49923e
Tim Kraken <tim.moyence@gmail.com>
2024-04-10 09:06:29 +0200

    config road and error handling road

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,34 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom, inject } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-
+import { Observable, catchError, throwError } from 'rxjs';
 import { routes } from './app.routes';
+import { ErrorHandlingService } from './core/adapters/errors/error-handling.service';
+
+function HttpErrorInterceptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<any>> {
+  return next(request).pipe(
+    catchError((error: HttpErrorResponse) => {
+      inject(ErrorHandlingService).handleError(error);
+      return throwError(() => error);
+    })
+  );
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    importProvidersFrom(BrowserModule),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([HttpErrorInterceptor])),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,16 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./views/homepage/homepage.component'),
   },
+  {
+    path: 'cities',
+    loadComponent: () => import('./views/city-list/city-list.component'),
+  },
+  {
+    path: 'forms/:id',
+    loadComponent: () => import('./views/forms/forms.component'),
+  },
+  {
+    path: 'form-create',
+    loadComponent: () => import('./views/form-create/form-create.component'),
+  },
 ];
